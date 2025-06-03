4fcfb8e50ac3244b59697d09f49e13ac7d9b0c24
dev_zach <zmohan@wustl.edu>
2025-06-02 21:30:23 -0400

    Adding recipe tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,12 @@
 """
 Tests for models.
 """
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -12,7 +16,10 @@ class ModelTests(TestCase):
         """Test creating a new user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -26,7 +33,10 @@ class ModelTests(TestCase):
             ['test4@example.COM', 'test4@example.com']
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email=email, password='sample123')
+            user = get_user_model().objects.create_user(
+                email=email,
+                password='sample123',
+            )
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
@@ -39,8 +49,23 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_superuser(
             'test@example.com',
             'test123',
-
         )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
