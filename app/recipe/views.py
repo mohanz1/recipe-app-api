4caf301892865bc6d0432d6ae32ba3653350b4c3
dev_zach <zmohan@wustl.edu>
2025-06-03 15:26:37 -0400

    detecting diff

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -46,3 +46,14 @@ class TagViewSet(
     def get_queryset(self):
         """Retrieve tags for authenticated users."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in the database."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        self.queryset.filter(user=self.request.user).order_by('-name')
